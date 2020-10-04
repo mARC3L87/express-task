c19d2e8e35495054d1e1246760f93383933faced
Marcel Łukomski <marcel.lukomski@gmail.com>
2020-10-04 15:23:17 +0200

    Init project

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
+
+const app = express();
+
+
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
+
+app.get('/', (req, res) => {
+  res.show('index.html');
+});
+
+app.get('/about', (req, res) => {
+  res.show('about.html');
+});
+
+app.get('/contact', (req, res) => {
+  res.show('contact.html');
+});
+
+app.get('/info', (req, res) => {
+  res.show('info.html');
+});
+
+app.get('/history', (req, res) => {
+  res.show('history.html');
+});
+
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+});
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
